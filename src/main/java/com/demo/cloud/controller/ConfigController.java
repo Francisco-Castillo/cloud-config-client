52fc1a878fe84c41629c2023d13f07053c1a0627
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2021-12-02 01:18:20 -0300

    commit inicial

---
+++ src/main/java/com/demo/cloud/controller/ConfigController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.demo.cloud.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author fcastillo
+ */
+@RestController
+@RequestMapping(value = "/profile")
+@CrossOrigin(origins = "*")
+public class ConfigController {
+
+  @Value("${user.role}")
+  private String role;
+
+  @GetMapping("/{name}")
+  public ResponseEntity<Object> getProfile(@PathVariable String name) {
+    Map<String, Object> response = new HashMap<>();
+    response.put("message", "Perfil activo: " + name + "\tRol: " + role);
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
+
+}
